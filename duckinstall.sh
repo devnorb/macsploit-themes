495d48afbd9d49389f2a81dde2cb89cc8f83c2ac
norb_ <127006485+devnorb@users.noreply.github.com>
2023-10-19 17:11:18 -0700

    Update duckinstall.sh

--- duckinstall.sh
+++ duckinstall.sh
@@ -2,7 +2,7 @@
 
 main() {
     cd /Applications/MacSploit.app/Contents/Resources/CodeViewer_CodeViewer.bundle/Contents/Resources/ace.bundle
-    echo -e "Downloading the Hacker theme..."
+    echo -e "Downloading the Duck theme..."
     curl "https://raw.githubusercontent.com/devnorb/macsploit-themes/main/duck.js" -o "./theme-tomorrow_night.js"
     echo -e "Theme install complete!"
 }
