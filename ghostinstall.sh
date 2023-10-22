a6c050d57700170a2237b137ef0c442196a5577e
norb_ <127006485+devnorb@users.noreply.github.com>
2023-10-22 13:51:56 -0700

    Create ghostinstall.sh

---
+++ ghostinstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+main() {
+    cd /Applications/MacSploit.app/Contents/Resources/CodeViewer_CodeViewer.bundle/Contents/Resources/ace.bundle
+    echo -e "Downloading the Ghost theme..."
+    curl "https://raw.githubusercontent.com/devnorb/macsploit-themes/main/ghost.js" -o "./theme-tomorrow_night.js"
+    echo -e "Theme install complete!"
+}
+
+main
